f95f7774fe1300d58f2733884073c913d6cdcae3
Arush Pimpalkar <arush.pimpalkar@gmail.com>
2024-10-19 14:36:51 +0530

    Update app_3.js

--- app_3.js
+++ app_3.js
@@ -1,2 +1,23 @@
-const userInput = "<script>alert('XSS')</script>";
-document.getElementById('output').innerHTML = userInput;
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'testdb'
+});
+
+app.get('/user', (req, res) => {
+  const userId = req.query.id;
+  const query = `SELECT * FROM users WHERE id = ${userId};`;
+
+  connection.query(query, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
